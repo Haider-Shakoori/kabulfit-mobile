8f85572abfc09dd6a667412f26370661c8315390
Haider-Shakoori <37241311+Haider-Shakoori@users.noreply.github.com>
2026-09-22 20:21:16 +0430

    fix: satisfy Flutter analysis and Android build gates

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,6 +12,10 @@ android {
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
+    buildFeatures {
+        resValues = true
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
